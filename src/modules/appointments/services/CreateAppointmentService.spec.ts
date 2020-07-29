ffa8516b82bcbe4a3bdc976b4d1070b36d900c6f
Alexandre <alexandrehrtm@protonmail.com>
2020-07-28 22:14:38 -0300

    tdd

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,27 @@
+import 'reflect-metadata';
+
+import CreateAppointmentService from './CreateAppointmentService';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppoitmentsRepository';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123');
+  });
+});
+
+// describe('CreateAppointment', () => {
+//   it('should be able to create a new appointment', () => {
+
+//   })
+// })
