061161eb372179c8e1df92afc4d8e8e9753a88c7
Alexandre <alexandrehrtm@protonmail.com>
2020-10-17 02:00:53 -0300

    filtrando agendamentos por mês

---
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -0,0 +1,51 @@
+import ListProviderMonthAvailabilityService from './ListProviderMonthAvailabilityService';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppoitmentsRepository';
+
+let listProviderMonthAvailability: ListProviderMonthAvailabilityService;
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+
+describe('ListProviderMonthAvailability', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderMonthAvailability = new ListProviderMonthAvailabilityService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the month availability from provier', async () => {
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 3, 20, 8, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 11, 20, 8, 0, 0), // year, month, day, hour, minute, seconds
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 11, 20, 8, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 11, 21, 8, 0, 0),
+    });
+
+    const availability = await listProviderMonthAvailability.execute({
+      provider_id: 'user',
+      year: 2020,
+      month: 12,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        { day: 19, available: true },
+        { day: 20, available: false },
+        { day: 21, available: false },
+        { day: 22, available: true },
+      ]),
+    );
+  });
+});
