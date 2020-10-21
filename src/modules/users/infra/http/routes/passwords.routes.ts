14746053f0accdd2c51985c33f7fa21a6a51feb4
Alexandre <alexandrehrtm@protonmail.com>
2020-10-21 15:59:49 -0300

    validando dados

--- src/modules/users/infra/http/routes/passwords.routes.ts
+++ src/modules/users/infra/http/routes/passwords.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ForgotPasswordController from '../controllers/ForgotPasswordController';
 import ResetPasswordController from '../controllers/ResetPasswordController';
@@ -8,7 +9,25 @@ const passwordRouter = Router();
 const forgotPasswordController = new ForgotPasswordController();
 const resetPassworController = new ResetPasswordController();
 
-passwordRouter.post('/forgot', forgotPasswordController.create);
-passwordRouter.post('/reset', resetPassworController.create);
+passwordRouter.post(
+  '/forgot',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+    },
+  }),
+  forgotPasswordController.create,
+);
+passwordRouter.post(
+  '/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+    },
+  }),
+  resetPassworController.create,
+);
 
 export default passwordRouter;
--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import UsersController from '../controllers/UsersController';
 import UserAvatarController from '../controllers/UserAvatarController';
@@ -13,15 +14,18 @@ const upload = multer(uploadConfig);
 const usersController = new UsersController();
 const userAvatarControllers = new UserAvatarController();
 
-/**
- * This route creates a new user
- * Requires name, email and password
- */
-usersRouter.post('/', usersController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
 
-/**
- * This route uploads user's profile picture
- */
 usersRouter.patch(
   '/avatar',
   ensureAuthenticated,
